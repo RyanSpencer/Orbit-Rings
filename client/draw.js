49f7f9808af14c2e8bae66b8ce2fd485f86b0bd9
Ryan Muskopf (RIT Student) <rjm9187@ad.rit.edu>
2017-05-03 14:09:38 -0400

    end game screen works

--- client/draw.js
+++ client/draw.js
@@ -4,173 +4,197 @@ let debug = false;
 //Draws everthing to the screen
 const drawCars = () => {
 
-  if(gameState === GAME_STATE.LOBBY){
+	if(gameState === GAME_STATE.LOBBY){
 
-    ctx.save();
-    ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    if(isHost){
-      fillText("Start when ready", WIDTH/2, HEIGHT/2, "20pt Jura", "white");
-    }else{
-      fillText("Waiting for host to start the game", WIDTH/2, HEIGHT/2, "20pt Jura", "white");
-    }
-    ctx.restore();
+		ctx.save();
+		ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		if(isHost){
+			fillText("Start when ready", WIDTH/2, HEIGHT/2, "20pt Jura", "white");
+		}else{
+			fillText("Waiting for host to start the game", WIDTH/2, HEIGHT/2, "20pt Jura", "white");
+		}
+		ctx.restore();
 
-  } else{
-    const deltaTime = calculateDeltaTime();
+	}else if(gameState === GAME_STATE.END){
+		ctx.save();
+		ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
 
-    moveCars(deltaTime);
+		fillText("Game Over", WIDTH/2, HEIGHT/2, "20pt Jura", "white");
 
-    checkCollisions(deltaTime);
+		ctx.restore();
+	} else{
+		const deltaTime = calculateDeltaTime();
 
-    //Draw background
-    ctx.save();
-    ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
+		moveCars(deltaTime);
 
-    //Draw the sun
-    ctx.translate(sun.x, sun.y);
-    if (debug) {
-      //Draw the area of gravitational effect if in debug
-      ctx.fillStyle = "yellow";
-      ctx.beginPath();
-      ctx.arc(0, 0, sun.size, 0, Math.PI * 2);
-      ctx.closePath();
-      ctx.fill();
-    }
-    //Draw the acual sun
-    ctx.fillStyle = "orange";
-    ctx.beginPath();
-    ctx.arc(0, 0, sun.core, 0, Math.PI * 2);
-    ctx.closePath();
-    ctx.fill();
-    if (debug) {
-      //If in debug, draw arrows to show direction of gravitational field
-      for (let i = 0; i < 4; i++) {
-        ctx.rotate(Math.PI/2);
-        ctx.strokeStyle = "black";
-        ctx.beginPath();
-        ctx.moveTo(-20, -60);
-        ctx.lineTo(20, -60);
-        ctx.lineTo(17, -63);
-        ctx.moveTo(20, -60);
-        ctx.lineTo(17, -57);
-        ctx.stroke();
-        ctx.closePath();
-      }
-    }
+		checkCollisions(deltaTime);
 
-    ctx.restore(); 
+		//Draw background
+		ctx.save();
+		ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
 
-    const keys = Object.keys(cars);
+		//Draw the sun
+		ctx.translate(sun.x, sun.y);
+		if (debug) {
+			//Draw the area of gravitational effect if in debug
+			ctx.fillStyle = "yellow";
+			ctx.beginPath();
+			ctx.arc(0, 0, sun.size, 0, Math.PI * 2);
+			ctx.closePath();
+			ctx.fill();
+		}
+		//Draw the acual sun
+		ctx.fillStyle = "orange";
+		ctx.beginPath();
+		ctx.arc(0, 0, sun.core, 0, Math.PI * 2);
+		ctx.closePath();
+		ctx.fill();
+		if (debug) {
+			//If in debug, draw arrows to show direction of gravitational field
+			for (let i = 0; i < 4; i++) {
+				ctx.rotate(Math.PI/2);
+				ctx.strokeStyle = "black";
+				ctx.beginPath();
+				ctx.moveTo(-20, -60);
+				ctx.lineTo(20, -60);
+				ctx.lineTo(17, -63);
+				ctx.moveTo(20, -60);
+				ctx.lineTo(17, -57);
+				ctx.stroke();
+				ctx.closePath();
+			}
+		}
 
-    //console.log(cars[keys[0]]);
+		ctx.restore(); 
 
-    for (let i = 0; i < keys.length; i++) {
-      const car = cars[keys[i]];
+		const keys = Object.keys(cars);
 
-      //If the car is dead don't draw it
-      if (car.state === CAR_STATE.DEAD) continue;
-      //Otherwise draw the car
-      ctx.save();
-      ctx.fillStyle = car.fillStyle;
-      ctx.fillRect(car.x, car.y, car.size * 2, car.size * 2);
-      ctx.restore();
-      if (debug) {
-        //Show the origin of each rectangle for developer aid
-        ctx.save();
-        ctx.translate(car.x, car.y);
-        ctx.beginPath();
-        ctx.fillStyle = "white";
-        ctx.arc(0, 0, 3, 0, Math.PI * 2);
-        ctx.closePath();
-        ctx.fill();
+		//console.log(cars[keys[0]]);
 
-        //Show velocity
-        ctx.beginPath();
-        ctx.globalAlpha = 0.7;
-        ctx.strokeStyle = "blue";
-        ctx.translate(car.size, car.size)
-        ctx.moveTo(0, 0);
-        ctx.lineTo(car.velocity.x * 10, car.velocity.y * 10);
-        ctx.closePath();
-        ctx.stroke();
+		let aliveCount = 0;
 
-        //show accleration
-        ctx.beginPath();
-        ctx.strokeStyle = "Red";
-        ctx.moveTo(0, 0);
-        ctx.lineTo(car.acceleration.x * 10, car.acceleration.y * 10);
-        ctx.closePath();                    
-        ctx.stroke();
-        ctx.restore();
-      }
-    }
+		for (let i = 0; i < keys.length; i++) {
+			const car = cars[keys[i]];
 
-    drawHUD();
-  }
+			console.log(car.state);
+
+			//If the car is dead don't draw it and add to dead c
+			if (car.state !== CAR_STATE.DEAD){
+
+				aliveCount++;
+
+				ctx.save();
+				ctx.fillStyle = car.fillStyle;
+				ctx.fillRect(car.x, car.y, car.size * 2, car.size * 2);
+				ctx.restore();
+				if (debug) {
+					//Show the origin of each rectangle for developer aid
+					ctx.save();
+					ctx.translate(car.x, car.y);
+					ctx.beginPath();
+					ctx.fillStyle = "white";
+					ctx.arc(0, 0, 3, 0, Math.PI * 2);
+					ctx.closePath();
+					ctx.fill();
+
+					//Show velocity
+					ctx.beginPath();
+					ctx.globalAlpha = 0.7;
+					ctx.strokeStyle = "blue";
+					ctx.translate(car.size, car.size)
+					ctx.moveTo(0, 0);
+					ctx.lineTo(car.velocity.x * 10, car.velocity.y * 10);
+					ctx.closePath();
+					ctx.stroke();
+
+					//show accleration
+					ctx.beginPath();
+					ctx.strokeStyle = "Red";
+					ctx.moveTo(0, 0);
+					ctx.lineTo(car.acceleration.x * 10, car.acceleration.y * 10);
+					ctx.closePath();                    
+					ctx.stroke();
+					ctx.restore();
+				}
+			}
 
 
-  animationFrame = requestAnimationFrame(drawCars);
+		}
+
+		if(isHost && aliveCount <= 1 && keys.length > 1){
+			console.log(keys.length);
+			console.log("1 or less players alive")
+			socket.emit('endGame');
+		}
+
+		drawHUD();
+	}
+
+
+	animationFrame = requestAnimationFrame(drawCars);
 };
 
 const drawHUD = () => {
 
-  ctx.save();
-  //Text for debug information
-  if (debug) {
-    fillText("Debug Info:Press N to toggle Debug", 10, 30, "20pt 'Exo 2'", "white");
-    
-    ctx.strokeStyle = 'white';
-    ctx.beginPath();
-    ctx.moveTo(canvas.width/2, 0);
-    ctx.lineTo(canvas.width/2, canvas.height);
-    ctx.stroke();
+	ctx.save();
+	//Text for debug information
+	if (debug) {
+		fillText("Debug Info:Press N to toggle Debug", 10, 30, "20pt 'Exo 2'", "white");
 
-    ctx.beginPath();
-    ctx.moveTo(0, canvas.height/2);
-    ctx.lineTo(canvas.width, canvas.height/2);
-    ctx.stroke();
-  }
-  //Car health stacked from the bottom dynamically so the last player will always next to the bottom of the canvas
-  const keys = Object.keys(cars);
-  for (let i = keys.length - 1; i >= 0 ; i--) {
-    if (cars[keys[i]].state === CAR_STATE.DEAD) continue;
-    fillText("Player " + (i+1) + " Population: " + cars[keys[i]].health.toFixed(1) + " million", 10, HEIGHT - ((keys.length - i) *30 ), "20pt 'Exo 2'", cars[keys[i]].fillStyle);
-  }
+		ctx.strokeStyle = 'white';
+		ctx.beginPath();
+		ctx.moveTo(canvas.width/2, 0);
+		ctx.lineTo(canvas.width/2, canvas.height);
+		ctx.stroke();
 
-  ctx.restore();
+		ctx.beginPath();
+		ctx.moveTo(0, canvas.height/2);
+		ctx.lineTo(canvas.width, canvas.height/2);
+		ctx.stroke();
+	}
+	//Car health stacked from the bottom dynamically so the last player will always next to the bottom of the canvas
+	const keys = Object.keys(cars);
+	for (let i = keys.length - 1; i >= 0 ; i--) {
+		if (cars[keys[i]].state === CAR_STATE.DEAD) continue;
+		fillText("Player " + (i+1) + " Population: " + cars[keys[i]].health.toFixed(1) + " million", 10, HEIGHT - ((keys.length - i) *30 ), "20pt 'Exo 2'", cars[keys[i]].fillStyle);
+	}
+
+	ctx.restore();
 };
 
 //Taken from Boomshine to display text easily
 const fillText = (string, x, y, css, color) => {
 
-  ctx.save();
-  // https://developer.mozilla.org/en-US/docs/Web/CSS/font
-  ctx.font = css;
-  ctx.fillStyle = color;
-  ctx.fillText(string, x, y);
-  ctx.restore();
+	ctx.save();
+	// https://developer.mozilla.org/en-US/docs/Web/CSS/font
+	ctx.font = css;
+	ctx.fillStyle = color;
+	ctx.fillText(string, x, y);
+	ctx.restore();
 };
 
 const drawIntroScreen = () =>{
-  setTimeout(()=>{   
-    ctx.save();
-    ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    fillText("Start or Join a Battle to Begin Playing", WIDTH/2, HEIGHT/2, "20pt Jura", "white");
-    ctx.restore();
-  }, 300);
+	setTimeout(()=>{   
+		ctx.save();
+		ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		fillText("Start or Join a Battle to Begin Playing", WIDTH/2, HEIGHT/2, "20pt Jura", "white");
+		ctx.restore();
+	}, 300);
 }
 
 const drawWaitingScreen = () =>{
-  setTimeout(()=>{   
-    ctx.save();
-    ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    fillText("Waiting", WIDTH/2, HEIGHT/2, "20pt Jura", "white");
-    ctx.restore();
-  }, 300);
+	setTimeout(()=>{   
+		ctx.save();
+		ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		fillText("Waiting", WIDTH/2, HEIGHT/2, "20pt Jura", "white");
+		ctx.restore();
+	}, 300);
 }
